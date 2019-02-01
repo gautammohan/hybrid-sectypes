4fb53dd8ace59e362f35fe4eac2038a0cae59ca3
Gautam Mohan <s.gautam.mohan@gmail.com>
2019-02-01 10:00:00 -0500

    Parse JSON encoding of a SLSF Model output from slsf2json.m
    This code does not parse expressions, or separate flows into lists of
    expressions, or separate guards/labels from transitions. It is only meant to
    differentiate high-level structures

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,59 @@
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
 import Lib
 import Control.Monad.Writer
+import Control.Monad
 import Data.Map (Map, empty)
+import Data.Aeson
+import Data.ByteString.Lazy.UTF8 (fromString)
+import qualified Data.Vector as V
 
 main :: IO ()
 main = someFunc
+
+instance FromJSON Expr where
+  parseJSON v = (pure Expr) <*> parseJSON v
+
+instance FromJSON Flow where
+  parseJSON v = (pure Flow) <*> parseJSON v
+
+instance FromJSON Mode where
+  parseJSON = withObject "mode object" $ \o ->
+    do
+      n <- o .: "name"
+      f <- o .: "flow"
+      pure $ Mode n (Flow [f])
+
+instance FromJSON Transition where
+  parseJSON = withObject "transition object" $ \o ->
+    do
+      g <- o .: "guard"
+      src <- parseJSON =<< o .: "src"
+      dest <- parseJSON =<< o .: "dest"
+      pure $ Transition src dest (Guard g) (Reset [Expr ""])
+
+instance FromJSON Model where
+  parseJSON = withObject "model object" $ \o ->
+    do
+      ty <- o .: "ty"
+      children <- o .: "children"
+      if (ty :: String) == "parmodel"
+        then
+        pure Parallel <*> mapM parseJSON children
+        else do
+        ts <- mapM parseJSON =<< o .: "transitions"
+        ms <- mapM parseJSON children
+        pure $ Model ms ts
 
 -- Basic Structure of a Hybrid system
 -- NOTE: Currently ignoring expression parsing
 type Var = String
 type Name = String
 
-data Expr = H | L deriving (Eq, Show)
+data Expr = Expr String deriving (Eq, Show)
 
 newtype Flow = Flow [Expr] deriving (Eq, Show)
 newtype Guard = Guard Expr deriving (Show)
@@ -39,8 +78,8 @@ validInContext :: Type -> Type -> Bool
 Low `validInContext` High = False
 _ `validInContext` _ = True
 
-data Error = Error Name String deriving (Show)
-type TC = Writer [Error]
+data Err = Err Name String deriving (Show)
+type TC = Writer [Err]
 
 class Checkable a where
   check :: Env -> a -> TC Type
@@ -49,8 +88,8 @@ instance Checkable [Expr] where
   check env exprs = foldr (liftM2 combineTypes) (return Low) (map (check env) exprs)
 
 instance Checkable Expr where
-  check _ H = return High
-  check _ L = return Low
+  check _ _ = return High
+  check _ _ = return Low
 
 instance Checkable Guard where
   check env (Guard expr) = check env expr
@@ -103,7 +142,7 @@ instance Checkable Model where
         ty2 <- check env t2
         if ty1 `validInContext` ty2
           then return ()
-          else tell [Error "[Name]" ("Transitions clash")]
+          else tell [Err "[Name]" ("Transitions clash")]
 
 -- A Simple Test Model
 
@@ -127,4 +166,13 @@ tr3 = Transition ml mh gh (Reset [])
 
 trA = Transition mh mh gh rh
 trB = Transition mh mh gl rl
-model = Model [mh,mh,mh] [trA,trB]
+
+-- Testing the JSON parser code
+
+s = fromString "{\"name\":\"Off1\",\"children\":[],\"ty\":\"mode\",\"flow\":\"Off1\ndu:\nx1_dot = -0.01*x1;\nx1_out = x1;\"}"
+
+s1 = fromString "{\"name\":\"Thermostat1\",\"children\":[{\"name\":\"Off1\",\"children\":[],\"ty\":\"mode\",\"flow\":\"Off1\ndu:\nx1_dot = -0.01*x1;\nx1_out = x1;\"},{\"name\":\"On1\",\"children\":[],\"ty\":\"mode\",\"flow\":\"On1\ndu:\nx1_dot = -0.01*(x1-100);\nx1_out = x1;\"}],\"ty\":\"regmodel\",\"decomposition\":\"EXCLUSIVE_OR\",\"transitions\":[{\"guard\":\"{x1=10}\",\"src\":[],\"dest\":\"On1\"},{\"guard\":\"[x1 >= 30]\",\"src\":\"On1\",\"dest\":\"Off1\"},{\"guard\":\"[x1 <= 20]\",\"src\":\"Off1\",\"dest\":\"On1\"}]}"
+
+s2 = fromString "{\"name\":\"Chart\",\"children\":[{\"name\":\"Thermostat1\",\"children\":[{\"name\":\"Off1\",\"children\":[],\"ty\":\"mode\",\"flow\":\"Off1\ndu:\nx1_dot = -0.01*x1;\nx1_out = x1;\"},{\"name\":\"On1\",\"children\":[],\"ty\":\"mode\",\"flow\":\"On1\ndu:\nx1_dot = -0.01*(x1-100);\nx1_out = x1;\"}],\"ty\":\"regmodel\",\"decomposition\":\"EXCLUSIVE_OR\",\"transitions\":[{\"guard\":\"{x1=10}\",\"src\":{\"name\":\"InitialTransition\",\"flow\":\"\",\"ty\":\"mode\"},\"dest\":{\"name\":\"On1\",\"ty\":\"mode\",\"flow\":\"On1\ndu:\nx1_dot = -0.01*(x1-100);\nx1_out = x1;\"}},{\"guard\":\"[x1 >= 30]\",\"src\":{\"name\":\"On1\",\"ty\":\"mode\",\"flow\":\"On1\ndu:\nx1_dot = -0.01*(x1-100);\nx1_out = x1;\"},\"dest\":{\"name\":\"Off1\",\"ty\":\"mode\",\"flow\":\"Off1\ndu:\nx1_dot = -0.01*x1;\nx1_out = x1;\"}},{\"guard\":\"[x1 <= 20]\",\"src\":{\"name\":\"Off1\",\"ty\":\"mode\",\"flow\":\"Off1\ndu:\nx1_dot = -0.01*x1;\nx1_out = x1;\"},\"dest\":{\"name\":\"On1\",\"ty\":\"mode\",\"flow\":\"On1\ndu:\nx1_dot = -0.01*(x1-100);\nx1_out = x1;\"}}]},{\"name\":\"Thermostat2\",\"children\":[{\"name\":\"Off2\",\"children\":[],\"ty\":\"mode\",\"flow\":\"Off2\ndu:\nx2_dot = -0.02*x2;\nx2_out = x2;\"},{\"name\":\"On2\",\"children\":[],\"ty\":\"mode\",\"flow\":\"On2\ndu:\nx2_dot = -0.02*(x2-100);\nx2_out = x2;\"}],\"ty\":\"regmodel\",\"decomposition\":\"EXCLUSIVE_OR\",\"transitions\":[{\"guard\":\"{x2=20}\n\",\"src\":{\"name\":\"InitialTransition\",\"flow\":\"\",\"ty\":\"mode\"},\"dest\":{\"name\":\"On2\",\"ty\":\"mode\",\"flow\":\"On2\ndu:\nx2_dot = -0.02*(x2-100);\nx2_out = x2;\"}},{\"guard\":\"[x2 <= 30]\",\"src\":{\"name\":\"Off2\",\"ty\":\"mode\",\"flow\":\"Off2\ndu:\nx2_dot = -0.02*x2;\nx2_out = x2;\"},\"dest\":{\"name\":\"On2\",\"ty\":\"mode\",\"flow\":\"On2\ndu:\nx2_dot = -0.02*(x2-100);\nx2_out = x2;\"}},{\"guard\":\"[x2 >= 50]\",\"src\":{\"name\":\"On2\",\"ty\":\"mode\",\"flow\":\"On2\ndu:\nx2_dot = -0.02*(x2-100);\nx2_out = x2;\"},\"dest\":{\"name\":\"Off2\",\"ty\":\"mode\",\"flow\":\"Off2\ndu:\nx2_dot = -0.02*x2;\nx2_out = x2;\"}}]}],\"ty\":\"parmodel\",\"decomposition\":\"PARALLEL_AND\",\"transitions\":[]}"
+
+s3 = fromString "{\"guard\":\"{x1=10}\",\"src\":{\"name\":\"InitialTransition\",\"flow\":\"\",\"ty\":\"mode\"},\"dest\":{\"name\":\"On1\",\"ty\":\"mode\",\"flow\":\"On1\ndu:\nx1_dot = -0.01*(x1-100);\nx1_out = x1;\"}}"
