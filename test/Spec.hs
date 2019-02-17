0b974518b7cbd7b6855870de94b08269b1087b6b
Gautam Mohan <s.gautam.mohan@gmail.com>
2019-02-17 12:34:45 -0500

    first unit tests added, drawing on HSpec and HUnit

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,18 @@
+import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.HUnit
+import Text.Parsec (parse)
+import Data.Either (isLeft)
+
+import Model
+import Parse
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main =
+  hspec $ do
+    describe "Parsing Var/Expr/Flow/Assignments works properly" $ do
+      it "parses MATLAB variables correctly" $ do
+        (parse var "" "B2_dot") @=? Right (Var "B2_dot")
+        (parse var "" "n2") @=? Right (Var "n2")
+        (parse var "" "23bad_var") `shouldSatisfy` isLeft
+        (parse var "" "") `shouldSatisfy` isLeft
