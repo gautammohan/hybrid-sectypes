968f016070517e3e19b9ead3ddeb2d21ecb1b317
Gautam Mohan <s.gautam.mohan@gmail.com>
2019-03-07 16:41:54 -0500

    Passing unit tests for Dependency Graphs

--- test/InferenceSpec.hs
+++ test/InferenceSpec.hs
@@ -4,10 +4,10 @@ import Test.Hspec
 import Control.Monad.State
 import Data.Map as M
 import Control.Exception (evaluate)
+import Data.Either
 
 import Inference
 import Model
-
 
 spec :: Spec
 spec = do
@@ -22,7 +22,8 @@ spec = do
       (counter st) `shouldBe` 3 --2 vars, 1 expr
       length (constraints st) `shouldBe` 2 --expr tyv relates to two var tyvs
     specify "assignments" $ do
-      let a = genConstraints (CAssn $ Assignment (Var "x2_dot") (Expr "-0.02*x2"))
+      let a =
+            genConstraints (CAssn $ Assignment (Var "x2_dot") (Expr "-0.02*x2"))
           (_, st) = runState a emptyState
       (counter st) `shouldBe` 4 --2 vars, 1 expr, 1 assn
       length (constraints st) `shouldBe` 3 --assn relates to 1var, 1expr + 1var
@@ -39,4 +40,46 @@ spec = do
           (_, st) = runState m emptyState
       (counter st) `shouldBe` 6 -- 2 vars, 2 exprs, 2 assignments, 1 flow, 1 mode
       length (constraints st) `shouldBe` 5 -- mode relates to flow + 4 assn
-  describe ""
+  describe "Dependency Graph Unit Tests" $ do
+      --- Preliminary Declarations for typing ease
+      let a = Var "a"
+          b = Var "b"
+          c = Var "c"
+          aplusb = Expr "a + b"
+          bplusc = Expr "b + c"
+          aplusc = Expr "a + c"
+          aplusbplusc = Expr "a + b + c"
+      ----- Actual Tests Start Here
+      specify "simple inference: a, a:High" $ do
+        let g = inferDepGraph (CVar a) [(a, High)]
+            result = checkDepGraph g
+        result `shouldSatisfy` isRight
+        let Right (m, r) = result
+        m !? (CVar a) `shouldBe` Just High
+      specify "simple inference: a = a + b, a:Low" $ do
+        let g = inferDepGraph (CAssn (Assignment a aplusb)) [(a, Low)]
+            result = checkDepGraph g
+        result `shouldSatisfy` isRight
+        let Right (m, r) = result
+        m !? (CVar b) `shouldBe` Just Low
+      specify "no inference possible: a = a + b, b:Low" $ do
+        let g = inferDepGraph (CAssn (Assignment a aplusb)) [(b, Low)]
+            result = checkDepGraph g
+        result `shouldSatisfy` isRight
+        let Right (m, r) = result
+        m !? (CVar a) `shouldBe` Nothing
+      specify "Invalid typing: a = a + b, a:Low b:High" $ do
+        let g =
+              inferDepGraph (CAssn (Assignment a aplusb)) [(a, Low), (b, High)]
+            result = checkDepGraph g
+        result `shouldSatisfy` isLeft
+      specify "Harder Inference: a = a + b, c = a + c, c:low" $ do
+        let g =
+              inferDepGraph
+                (CFlow (Flow [Assignment a aplusb, Assignment c aplusc]))
+                [(c, Low)]
+            result = checkDepGraph g
+        result `shouldSatisfy` isRight
+        let Right (m, r) = result
+        m !? (CVar a) `shouldBe` Just Low
+        m !? (CVar b) `shouldBe` Just Low
